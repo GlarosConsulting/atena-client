d0360464ae09249659b35aa6c753ba217e7dd61b
André 'Dezzy <andrevcv1@gmail.com>
2020-09-24 11:16:11 -0300

    feat: Add Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
-import { ThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ReactGA from 'react-ga';
+
+import { ThemeProvider } from '@material-ui/core/styles';
 
 import 'typeface-roboto';
 
@@ -12,6 +14,11 @@ import AppProvider from './contexts';
 import Routes from './routes';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    ReactGA.initialize('UA-178895843');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <>
       <Helmet>
